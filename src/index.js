6d736657519956351a8ac29e419deccdc2d6d822
l1nky358 <prokotrob@gmail.com>
2026-01-13 17:54:51 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,23 +1,14 @@
-import { readFileSync } from 'fs';
-import { extname } from 'path';
-import parse from '../src/parsers.js';
-import buildDiff from '../src/buildTree.js';
-import getFormatter from '../src/formatters/index.js';
+import fs from 'fs';
+import yaml from 'yaml';
 
-const getFormat = (filepath) => extname(filepath).slice(1);
-const readFile = (filepath) => readFileSync(filepath, 'utf-8');
-
-const genDiff = (filepath1, filepath2, formatName = 'stylish') => {
-  const format1 = getFormat(filepath1);
-  const format2 = getFormat(filepath2);
-  
-  const data1 = parse(readFile(filepath1), format1);
-  const data2 = parse(readFile(filepath2), format2);
-  
-  const diff = buildDiff(data1, data2);
-  const formatter = getFormatter(formatName);
-  
-  return formatter(diff);
+const parse = (content, format) => {
+  if (format === 'json') {
+    return JSON.parse(content);
+  }
+  if (format === 'yml' || format === 'yaml') {
+    return yaml.parse(content);
+  }
+  throw new Error(`Unsupported format: ${format}`);
 };
 
-export default genDiff;
+export default parse;
