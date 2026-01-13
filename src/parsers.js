12023437dac86a2e655c8fe30431193e44959a84
l1nky358 <prokotrob@gmail.com>
2026-01-13 17:55:33 +0700

    Update parsers.js

--- src/parsers.js
+++ src/parsers.js
@@ -1,10 +1,6 @@
 import fs from 'fs';
 import yaml from 'yaml';
 
-const getFileContent = (filePath) => {
-  return fs.readFileSync(filePath, 'utf-8');
-};
-
 const parse = (content, format) => {
   if (format === 'json') {
     return JSON.parse(content);
@@ -15,10 +11,10 @@ const parse = (content, format) => {
   throw new Error(`Unsupported format: ${format}`);
 };
 
-const getData = (filePath) => {
-  const content = getFileContent(filePath);
+export const getData = (filePath) => {
+  const content = fs.readFileSync(filePath, 'utf-8');
   const format = filePath.split('.').pop().toLowerCase();
   return parse(content, format);
 };
 
-export default getData;
+export default parse;
