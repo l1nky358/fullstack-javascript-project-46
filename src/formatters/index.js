bd457b84cd122f7a4326bc7000a0ce129a297f82
l1nky358 <prokotrob@gmail.com>
2026-01-13 17:48:53 +0700

    Update index.js

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,6 +1,6 @@
-import formatStylish from '../src/formatters/stylish.js';
-import formatPlain from '../src/formatters/plain.js';
-import formatJson from '../src/formatters/json.js';
+import formatStylish from '../formatters/stylish.js';
+import formatPlain from '../formatters/plain.js';
+import formatJson from '../formatters/json.js';
 
 const formatters = {
   stylish: formatStylish,
