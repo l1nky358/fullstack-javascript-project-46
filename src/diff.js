4af9109df09a9e7846f2416fdc36e8562d2f15c4
l1nky358 <prokotrob@gmail.com>
2026-01-30 15:20:56 +0700

    Update diff.js

--- src/diff.js
+++ src/diff.js
@@ -1,49 +1,49 @@
 const buildDiff = (data1, data2) => {
-  const keys = new Set([...Object.keys(data1), ...Object.keys(data2)]);
-  const sortedKeys = Array.from(keys).sort();
+  const keys = new Set([...Object.keys(data1), ...Object.keys(data2)])
+  const sortedKeys = Array.from(keys).sort()
 
-  return sortedKeys.map((key) => {
-    const value1 = data1[key];
-    const value2 = data2[key];
+  return sortedKeys.map(key => {
+    const value1 = data1[key]
+    const value2 = data2[key]
 
     if (!(key in data1)) {
-      return { key, value: value2, type: 'added' };
+      return { key, value: value2, type: 'added' }
     }
     
     if (!(key in data2)) {
-      return { key, value: value1, type: 'removed' };
+      return { key, value: value1, type: 'removed' }
     }
     
     if (value1 === value2) {
-      return { key, value: value1, type: 'unchanged' };
+      return { key, value: value1, type: 'unchanged' }
     }
     
     return { 
       key, 
       oldValue: value1, 
       value: value2, 
-      type: 'changed' 
-    };
-  });
-};
+      type: 'changed',
+    }
+  })
+}
 
-export const formatDiff = (diff) => {
-  const lines = diff.map((item) => {
+export const formatDiff = diff => {
+  const lines = diff.map(item => {
     switch (item.type) {
       case 'added':
-        return `  + ${item.key}: ${item.value}`;
+        return `  + ${item.key}: ${item.value}`
       case 'removed':
-        return `  - ${item.key}: ${item.value}`;
+        return `  - ${item.key}: ${item.value}`
       case 'changed':
-        return `  - ${item.key}: ${item.oldValue}\n  + ${item.key}: ${item.value}`;
+        return `  - ${item.key}: ${item.oldValue}\n  + ${item.key}: ${item.value}`
       case 'unchanged':
-        return `    ${item.key}: ${item.value}`;
+        return `    ${item.key}: ${item.value}`
       default:
-        return '';
+        return ''
     }
-  });
+  })
 
-  return `{\n${lines.join('\n')}\n}`;
-};
+  return `{\n${lines.join('\n')}\n}`
+}
 
-export default buildDiff;
+export default buildDiff
