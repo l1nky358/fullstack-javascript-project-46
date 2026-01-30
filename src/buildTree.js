a76473a8380dc9733740c1bb2c5914f67ef89d86
l1nky358 <prokotrob@gmail.com>
2026-01-30 15:19:59 +0700

    Update buildTree.js

--- src/buildTree.js
+++ src/buildTree.js
@@ -1,13 +1,13 @@
-const isObject = (value) => 
-  typeof value === 'object' && value !== null && !Array.isArray(value);
+const isObject = value =>
+  typeof value === 'object' && value !== null && !Array.isArray(value)
 
 const buildTree = (data1, data2) => {
-  const keys = new Set([...Object.keys(data1), ...Object.keys(data2)]);
-  const sortedKeys = Array.from(keys).sort();
+  const keys = new Set([...Object.keys(data1), ...Object.keys(data2)])
+  const sortedKeys = Array.from(keys).sort()
 
-  return sortedKeys.map((key) => {
-    const value1 = data1[key];
-    const value2 = data2[key];
+  return sortedKeys.map(key => {
+    const value1 = data1[key]
+    const value2 = data2[key]
 
     if (!(key in data1)) {
       return {
@@ -15,7 +15,7 @@ const buildTree = (data1, data2) => {
         value: value2,
         type: 'added',
         children: isObject(value2) ? buildTree({}, value2) : [],
-      };
+      }
     }
     
     if (!(key in data2)) {
@@ -24,7 +24,7 @@ const buildTree = (data1, data2) => {
         value: value1,
         type: 'removed',
         children: isObject(value1) ? buildTree(value1, {}) : [],
-      };
+      }
     }
     
     if (isObject(value1) && isObject(value2)) {
@@ -32,7 +32,7 @@ const buildTree = (data1, data2) => {
         key,
         type: 'nested',
         children: buildTree(value1, value2),
-      };
+      }
     }
     
     if (value1 === value2) {
@@ -40,7 +40,7 @@ const buildTree = (data1, data2) => {
         key,
         value: value1,
         type: 'unchanged',
-      };
+      }
     }
     
     return {
@@ -48,8 +48,8 @@ const buildTree = (data1, data2) => {
       oldValue: value1,
       value: value2,
       type: 'changed',
-    };
-  });
-};
+    }
+  })
+}
 
-export default buildTree;
+export default buildTree
