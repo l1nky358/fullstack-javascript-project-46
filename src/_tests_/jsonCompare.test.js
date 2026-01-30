bb95d3a1db6d5ff2bf71313bd6e3d5ab446c7148
l1nky358 <prokotrob@gmail.com>
2026-01-30 15:17:50 +0700

    Update jsonCompare.test.js

--- src/_tests_/jsonCompare.test.js
+++ src/_tests_/jsonCompare.test.js
@@ -1,17 +1,17 @@
-import fs from 'fs';
+import fs from 'fs'
 
 const compareJsonFiles = async (filePath1, filePath2) => {
-  const json1 = JSON.parse(await fs.readFile(filePath1, 'utf8'));
-  const json2 = JSON.parse(await fs.readFile(filePath2, 'utf8'));
-  return JSON.stringify(json1) === JSON.stringify(json2);
-};
+  const json1 = JSON.parse(await fs.readFile(filePath1, 'utf8'))
+  const json2 = JSON.parse(await fs.readFile(filePath2, 'utf8'))
+  return JSON.stringify(json1) === JSON.stringify(json2)
+}
 
 test('сравнение двух одинаковых json-файлов', async () => {
-  const result = await compareJsonFiles('./__fixtures__/file1.json', './__fixtures__/file2.json');
-  expect(result).toBe(true);
-});
+  const result = await compareJsonFiles('./__fixtures__/file1.json', './__fixtures__/file2.json')
+  expect(result).toBe(true)
+})
 
 test('сравнение двух разных json-файлов', async () => {
-  const result = await compareJsonFiles('./__fixtures__/file1.json', './__fixtures__/file3.json');
-  expect(result).toBe(false);
-});
+  const result = await compareJsonFiles('./__fixtures__/file1.json', './__fixtures__/file3.json')
+  expect(result).toBe(false)
+})
